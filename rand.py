2b4e0a8fd92dea269c27088f157ee2acb3c7f426
Jesse Mazis <recursiveowl@gmail.com>
2016-09-07 21:12:36 +1000

    Added weightedRand function

---
+++ rand.py
@@ -0,0 +1,13 @@
+import random
+
+def weightedSelect (elements, weights):
+	totalWeight = sum (weights)
+	r = random.random () * totalWeight
+	index = 0
+	print (r)
+	for w in weights:
+		r -= w
+		if r<= 0:
+			return elements [index]
+		index += 1
+	return elements [-1]
