13c5ac1a6dc867477b9ed678f407665bdeca4eba
Jesse Mazis <recursiveowl@gmail.com>
2016-09-07 21:45:35 +1000

    Implemented MVP

--- data.py
+++ data.py
@@ -5,12 +5,16 @@ def load (fileName):
 	with open (fileName, newline='') as csvfile:
 		rows = csv.reader (csvfile, delimiter=',', quotechar='"')
 		columns = rows.__next__ ()
+		i=0
+		for c in columns:
+			columns [i] = c.strip ()
+			i += 1
 		
 		for r in rows:
 			index = 0
 			c = {}
 			for e in r:
-				c [columns [index]] = e
+				c [columns [index]] = e.strip ()
 				index += 1
 			dat.append (c)
 	return dat
--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 
 import data
+import rand
 
-print (data.load ('Frequency sheet - Classes.csv'))
+character = {}
+
+classes = data.load ('Frequency sheet - Classes.csv')
+races = data.load ('Frequency sheet - Races.csv')
+
+names, weights = data.extractNamesWeights (classes)
+character ['class'] = rand.weightedSelect (names, weights)
+
+names, weights = data.extractNamesWeights (races)
+character ['race'] = rand.weightedSelect (names, weights)
+
+print (character)
